796589342bf845752b2831e8924cb5597d382c1b
afdsj <thdla3441@gmail.com>
2023-09-01 16:46:31 +0900

    Entity 추가

--- src/main/java/com/teddybear6/toegeungil/notice/entity/Notice.java
+++ src/main/java/com/teddybear6/toegeungil/notice/entity/Notice.java
@@ -32,5 +32,85 @@ public class Notice {
     @Column(name = "notice_state")  // 공지 상태
     private String noticeState;
 
-    
+    public Notice() {
+    }
+
+    public Notice(int num, int noticeNum, String noticeTitle, String noticeContent, Date noticeDate, Date noticeModiDate, String noticeState) {
+        this.num = num;
+        this.noticeNum = noticeNum;
+        this.noticeTitle = noticeTitle;
+        this.noticeContent = noticeContent;
+        this.noticeDate = noticeDate;
+        this.noticeModiDate = noticeModiDate;
+        this.noticeState = noticeState;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public int getNoticeNum() {
+        return noticeNum;
+    }
+
+    public void setNoticeNum(int noticeNum) {
+        this.noticeNum = noticeNum;
+    }
+
+    public String getNoticeTitle() {
+        return noticeTitle;
+    }
+
+    public void setNoticeTitle(String noticeTitle) {
+        this.noticeTitle = noticeTitle;
+    }
+
+    public String getNoticeContent() {
+        return noticeContent;
+    }
+
+    public void setNoticeContent(String noticeContent) {
+        this.noticeContent = noticeContent;
+    }
+
+    public Date getNoticeDate() {
+        return noticeDate;
+    }
+
+    public void setNoticeDate(Date noticeDate) {
+        this.noticeDate = noticeDate;
+    }
+
+    public Date getNoticeModiDate() {
+        return noticeModiDate;
+    }
+
+    public void setNoticeModiDate(Date noticeModiDate) {
+        this.noticeModiDate = noticeModiDate;
+    }
+
+    public String getNoticeState() {
+        return noticeState;
+    }
+
+    public void setNoticeState(String noticeState) {
+        this.noticeState = noticeState;
+    }
+
+    @Override
+    public String toString() {
+        return "Notice{" +
+                "num=" + num +
+                ", noticeNum=" + noticeNum +
+                ", noticeTitle='" + noticeTitle + '\'' +
+                ", noticeContent='" + noticeContent + '\'' +
+                ", noticeDate=" + noticeDate +
+                ", noticeModiDate=" + noticeModiDate +
+                ", noticeState='" + noticeState + '\'' +
+                '}';
+    }
 }
