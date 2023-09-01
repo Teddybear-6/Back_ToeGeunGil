802b55155b61748c06b00e1ec0b70c4d63b9deff
afdsj <thdla3441@gmail.com>
2023-09-01 15:37:05 +0900

    공지사항 dto 생성

---
+++ src/main/java/com/teddybear6/toegeungil/notice/dto/NoticeDTO.java
@@ -0,0 +1,13 @@
+package com.teddybear6.toegeungil.notice.dto;
+
+
+import java.sql.Date;
+
+public class NoticeDTO {
+    private int noticeNum; // 공지 번호
+    private String noticeTitle; // 공지 제목
+    private Date noticeDate; // 공지 작성 날짜
+
+
+
+}
