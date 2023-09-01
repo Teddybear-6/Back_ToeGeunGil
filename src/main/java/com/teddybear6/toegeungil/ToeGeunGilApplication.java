edd14a331012cff90b96e84a4cc122713950ae2e
bearnyong <rhalsdud216@gmail.com>
2023-09-01 13:08:02 +0900

    환경설정

---
+++ src/main/java/com/teddybear6/toegeungil/ToeGeunGilApplication.java
@@ -0,0 +1,13 @@
+package com.teddybear6.toegeungil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToeGeunGilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToeGeunGilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teddybear6/toegeungil/ToeGeunGilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teddybear6.toegeungil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToeGeunGilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
