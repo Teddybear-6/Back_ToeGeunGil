84cdee72ce41c59eadbc2c8da6cf6d236e9d010a
danb0308 <danb0308@naver.com>
2023-09-03 21:05:49 +0900

    CommunityDTO 생성

---
+++ src/main/java/com/teddybear6/toegeungil/community/dto/CommunityDTO.java
@@ -0,0 +1,16 @@
+package com.teddybear6.toegeungil.community.dto;
+
+import java.util.Date;
+
+public class CommunityDTO {
+
+    private int communityId; // 소통 아이디
+    private String title; // 소통 제목
+    private String intro; // 소통 내용
+    private int categoryId; // 카테고리 아이디
+    private int keywordId; // 키워드 아이디
+    private int locationId; // 지역 아이디
+    private String status; //상태
+    private Date date; // 날짜
+}
+
