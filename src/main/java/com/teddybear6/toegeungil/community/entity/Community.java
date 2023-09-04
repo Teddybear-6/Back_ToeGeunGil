ba111ccb9c7b44240f1018c2ae0a88c3a281fa2c
danb0308 <danb0308@naver.com>
2023-09-04 09:46:04 +0900

    community Entity 수정

--- src/main/java/com/teddybear6/toegeungil/community/entity/Community.java
+++ src/main/java/com/teddybear6/toegeungil/community/entity/Community.java
@@ -1,27 +1,33 @@
 package com.teddybear6.toegeungil.community.entity;
 
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 
+@Entity
+@Table(name = "community")
 public class Community {
 
     @Id
-    @Column(name = "community_id")
-    private int communityId;
-    @Column(name = "title")
-    private String title;
-    @Column(name = "intro")
-    private String intro;
-    @Column(name = "categoryId")
-    private int categoryId;
-    @Column(name = "keywordId")
-    private int keywordId;
-    @Column(name = "locationId")
+    @Column(name = "community_num") // 커뮤니티 번호
+    private int communityNum;
+    @Column(name = "community_title") // 커뮤니티 제목
+    private String communityTitle;
+    @Column(name = "community_intro") // 커뮤니티 소개
+    private String communityIntro;
+    @Column(name = "category_num") // 카테고리 번호
+    private int categoryNum;
+    @Column(name = "keyword_num") // 키워드 번호
+    private int keywordNum;
+    @Column(name = "location_num") // 지역 번호
     private int locationId;
-    @Column(name = "status")
-    private String status;
-    @Column(name = "date")
-    private Date date;
+    @Column(name = "community_status") // 커뮤니티 상태
+    private String communityStatus;
+    @Column(name = "post_write_date") // 커뮤니티 작성일
+    private Date postWriteDate;
+    @Column(name = "post_update_date") // 커뮤니티 수정일
+    private Date postUpdateDate;
 }
