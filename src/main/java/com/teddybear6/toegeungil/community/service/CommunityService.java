042a22990e99198649d26f2b2551a44c32ccfde7
danb0308 <danb0308@naver.com>
2023-09-04 11:16:38 +0900

    CommunityService 생성

---
+++ src/main/java/com/teddybear6/toegeungil/community/service/CommunityService.java
@@ -0,0 +1,19 @@
+package com.teddybear6.toegeungil.community.service;
+
+import com.teddybear6.toegeungil.community.entity.Community;
+import com.teddybear6.toegeungil.community.repository.CommunityRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommunityService {
+    private final CommunityRepository communityRepository;
+
+    public CommunityService(CommunityRepository communityRepository) {
+        this.communityRepository = communityRepository;
+    }
+
+    public Community communityFindCode(int communityCode){
+        Community community = communityRepository.findById(communityCode);
+        return community;
+    }
+}
