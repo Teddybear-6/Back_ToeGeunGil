3e0365f885c473ee67a3e2c3f5f1053838a20946
danb0308 <danb0308@naver.com>
2023-09-04 12:30:45 +0900

    CommunityDTO 수정

--- src/main/java/com/teddybear6/toegeungil/community/controller/CommunityController.java
+++ src/main/java/com/teddybear6/toegeungil/community/controller/CommunityController.java
@@ -1,17 +1,15 @@
 package com.teddybear6.toegeungil.community.controller;
 
+import com.teddybear6.toegeungil.community.dto.CommunityDTO;
 import com.teddybear6.toegeungil.community.entity.Community;
 import com.teddybear6.toegeungil.community.service.CommunityService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
 
-@Controller
-@RequestMapping("/community") // 요청이 왔을 때 호출되는 주소
+@RestController
+@RequestMapping("/communitys")
 public class CommunityController {
 
     private final CommunityService communityService;
@@ -19,16 +17,20 @@ public class CommunityController {
     public CommunityController(CommunityService communityService) {
         this.communityService = communityService;
     }
+
     @GetMapping("/{communityCode}")
-    public ResponseEntity<Object> communityFindCode(@PathVariable int communityCode){
-        Community community = CommunityService.communityFindCode(communityCode);
+    public ResponseEntity<Object> findByCommunityCode(@PathVariable int communityCode){
+        Community community = communityService.findByCommunityCode(communityCode);
 
         if(Objects.isNull(community)){
             return ResponseEntity.status(404).body(new String("잘못된 코드 입력"));
         }
-        return ResponseEntity.ok().body(community);
-    }
 
+        CommunityDTO communityDTO = new CommunityDTO(community);
+
+        return ResponseEntity.ok().body(communityDTO);
+    }
 }
+
 
 
