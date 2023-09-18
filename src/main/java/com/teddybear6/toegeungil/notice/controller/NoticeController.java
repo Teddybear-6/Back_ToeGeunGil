f76392d2e34bb59eb74abe9b3e6149725052e298
afdsj <thdla3441@gmail.com>
2023-09-18 20:54:33 +0900

    NoticeController_Post 수정

--- src/main/java/com/teddybear6/toegeungil/notice/controller/NoticeController.java
+++ src/main/java/com/teddybear6/toegeungil/notice/controller/NoticeController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
@@ -48,7 +49,9 @@ public class NoticeController {
 
     /* <POST> /notices: 공지사항 등록 */
     @PostMapping
-    public ResponseEntity<?> registNotice(Notice notice) {
+    public ResponseEntity<?> registNotice(@RequestBody Notice notice) {
+        System.out.println(notice);
+        notice.setNoticeDate(new Date());
         int result = noticeService.registNotice(notice);
 
         if (result > 0) {
