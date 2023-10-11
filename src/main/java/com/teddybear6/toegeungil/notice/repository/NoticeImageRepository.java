79ecb1d25c73e687e40bf98e59fc343c3e72e4dd
afdsj <thdla3441@gmail.com>
2023-10-11 21:02:34 +0900

    NoticeImageRepository 생성

--- src/main/java/com/teddybear6/toegeungil/notice/repository/NoticeImageRepository.java
+++ src/main/java/com/teddybear6/toegeungil/notice/repository/NoticeImageRepository.java
@@ -1,4 +1,8 @@
 package com.teddybear6.toegeungil.notice.repository;
 
-public class NoticeImageRepository {
+import com.teddybear6.toegeungil.notice.entity.NoticeImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeImageRepository extends JpaRepository<NoticeImage, Integer> {
+    NoticeImage findByNoticeNum(int noticeNum);
 }
